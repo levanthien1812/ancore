7aac3b6af4bee32d1209c7cf0b6dd1c74f3e0a62
Le Van Thien <20521947@gm.uit.edu.vn>
2025-12-15 01:08:06 +0700

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    // This glob pattern ensures the specific generated Prisma directory is included
-    "./lib/generated/prisma": ["./lib/generated/prisma/**"],
-    // You might also need to include the standard node_modules client location if your setup uses it:
-    "/*": ["./node_modules/.prisma/client/**/*"],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
