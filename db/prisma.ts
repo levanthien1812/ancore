9180cec1461139354404f7f726fcb65d14573e3b
Le Van Thien <20521947@gm.uit.edu.vn>
2025-12-29 22:00:58 +0700

    update dependencies

--- db/prisma.ts
+++ db/prisma.ts
@@ -17,7 +17,11 @@ if (process.env.NODE_ENV === "development") {
   neonConfig.webSocketConstructor = ws.WebSocket;
 }
 
-const connectionString = `${process.env.DATABASE_URL}`;
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not defined");
+}
+
+const connectionString = process.env.DATABASE_URL;
 const adapter = new PrismaNeon({ connectionString });
 
 export const prisma =
