5fa93df61a6ae94860d2fd5ff54c14ec6b08318d
Le Van Thien <20521947@gm.uit.edu.vn>
2025-12-29 21:43:57 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/.prisma/client/**/*"],
+    "/*": ["./node_modules/.prisma/client/**/*"],
+  },
+};
 
 export default nextConfig;
