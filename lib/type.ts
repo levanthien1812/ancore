6739b889472f5e431e2a50b00ef7f29da0d352f7
Le Van Thien <20521947@gm.uit.edu.vn>
2026-05-13 11:20:58 +0700

    fix wrong field type

--- lib/type.ts
+++ lib/type.ts
@@ -56,6 +56,6 @@ export type Message = {
   content: string;
   refinement?: string | null;
   explanation?: string | null;
-  evaluation?: string;
+  evaluation?: string | null;
   speakingSuggestions?: string[];
 };
